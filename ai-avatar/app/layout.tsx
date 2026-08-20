912113c168377d8068e6856c6bc4ef2ef9028ada
Parkin Arsanamanee <parkinnauto@gmail.com>
2026-08-20 12:32:00 +0700

    Edit ai-avatar website's name and logo

--- ai-avatar/app/layout.tsx
+++ ai-avatar/app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "AI Robot Assistant",
+  description: "AI Robot Voice Assistant",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
